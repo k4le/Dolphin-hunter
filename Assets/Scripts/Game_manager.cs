42c255c3235784c596c09cd4c5b7669d570401ea
Kalle Kurki <kallekurki@kamk.fi>
2021-04-25 15:08:31 +0300

    Update scores to menu and hut

--- Assets/Scripts/Game_manager.cs
+++ Assets/Scripts/Game_manager.cs
@@ -6,18 +6,27 @@ public class Game_manager : MonoBehaviour
 {
 	GameObject newScoreObject;
 	GameObject hightScoreObject;
+	GameObject currentScoreObject;
 
 	public int highScore = 0;
 	public int newScore = 0;
 
 	void Start()
 	{
-
+		LoadScores();
 		newScoreObject = GameObject.FindGameObjectWithTag("newScore");
 		hightScoreObject = GameObject.FindGameObjectWithTag("highScore");
+		currentScoreObject = GameObject.FindGameObjectWithTag("currentScore");
+		newScoreObject.GetComponent<Text>().text = newScore.ToString();
+		hightScoreObject.GetComponent<Text>().text = highScore.ToString();
 	}
 	public void OnEndOfGame()
 	{
+		if (newScore > highScore)
+        {
+			highScore = newScore;
+        }
+		SaveScores();
 		newScoreObject.GetComponent<Text>().text = newScore.ToString();
 		if (newScore > highScore) {
 			highScore = newScore;
@@ -30,6 +39,23 @@ public class Game_manager : MonoBehaviour
 
 	internal void updateScore(int v)
 	{
+		currentScoreObject.GetComponent<Text>().text = newScore.ToString();
 		newScore += v;
+
+
+	}
+
+	void SaveScores()
+    {
+		PlayerPrefs.SetInt("newScore", newScore);
+		PlayerPrefs.SetInt("highScore", highScore);
+	}
+
+	void LoadScores()
+    {
+		newScore = PlayerPrefs.GetInt("newScore");
+		highScore = PlayerPrefs.GetInt("highScore");
+
+
 	}
 }
