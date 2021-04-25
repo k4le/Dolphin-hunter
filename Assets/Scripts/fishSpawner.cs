644daf6c1ac8a521e0b2cd44821a66c1e4860a35
Kalle Kurki <kallekurki@kamk.fi>
2021-04-25 16:59:53 +0300

    Add fish scaling multiplier

--- Assets/Scripts/fishSpawner.cs
+++ Assets/Scripts/fishSpawner.cs
@@ -7,6 +7,7 @@ public class fishSpawner : MonoBehaviour
 	public int fishAmount;
 	public int rangeX;
 	public int rangeY;
+	public float scalingMultiplier = 1;
 	public GameObject[] fishPrefab;
 
 	void Start()
@@ -14,7 +15,7 @@ public class fishSpawner : MonoBehaviour
 		for (int i = 0; i < fishAmount; i++) {
 			GameObject fish = Instantiate(fishPrefab[Random.Range(0, fishPrefab.Length)], new Vector3(UnityEngine.Random.Range(0, rangeX), UnityEngine.Random.Range(0, -rangeY) - 5, 0), transform.rotation);
 			float scaling = Mathf.Abs(fish.transform.position.y) / rangeY;
-			fish.transform.localScale *= (1 + scaling);
+			fish.transform.localScale *= (1 + scaling)* scalingMultiplier;
 		}
 	}
 }
