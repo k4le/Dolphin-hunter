5f741160a1a87ac2a786a379cb1caddf6c31c398
nea <nealuurila@kamk.fi>
2021-04-25 13:58:36 +0300

    game over ja lisää kaloja

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public GameObject menu;
+    public GameObject gameOver;
 
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 0f;
+        gameOver.SetActive(false);
     }
 
     public void Play()
@@ -19,6 +21,11 @@ public class MainMenu : MonoBehaviour
         menu.SetActive(false);
     }
 
+    public void GameOverToMenu()
+    {
+        menu.SetActive(true);
+        gameOver.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
